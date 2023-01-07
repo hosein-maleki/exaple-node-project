75808e7b7397357a05e4a684feccc5711b81f1ea
hosein maleki <hosein.maleki969@gmail.com>
2023-01-07 18:22:31 +0330

    update

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
-app.use("/api/product", product);
+
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
 });
+
+app.use("/api/product", product);
 
 module.exports = app;
